a9d2d406eb5436224ec30066bab1cee9c19436d6
MaoZiming <ziming.mao@yale.edu>
2024-02-07 06:07:55 +0000

    disable backpressure policy

--- python/ray/data/_internal/execution/backpressure_policy/__init__.py
+++ python/ray/data/_internal/execution/backpressure_policy/__init__.py
@@ -11,7 +11,8 @@ if TYPE_CHECKING:
 # Default enabled backpressure policies and its config key.
 # Use `DataContext.set_config` to config it.
 # TODO(hchen): Enable StreamingOutputBackpressurePolicy by default.
-ENABLED_BACKPRESSURE_POLICIES = [ConcurrencyCapBackpressurePolicy]
+# Disable StreamingOutputBackpressurePolicy for now.
+ENABLED_BACKPRESSURE_POLICIES = []
 ENABLED_BACKPRESSURE_POLICIES_CONFIG_KEY = "backpressure_policies.enabled"
 
 
